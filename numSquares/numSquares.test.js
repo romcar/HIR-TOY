3dd7f3f42313227342bedbc5935351c1a4cf9bde
romcar <rommel.carrasquilla@gmail.com>
2018-08-10 12:44:44 -0400

    Create test for numSquares

--- numSquares/numSquares.test.js
+++ numSquares/numSquares.test.js
@@ -5,12 +5,13 @@ describe('Number of Squares', () => {
 		expect(numSquares(144)).toBe(1); // 144
 		expect(numSquares(36)).toBe(1); // 36
 		expect(numSquares(1156)).toBe(1); // 1156 => 34^2
+		expect(numSquares(841)).toBe(1); // 400 + 441
 		expect(numSquares(145)).toBe(2); // 1 + 144
 		expect(numSquares(13)).toBe(2); // 4 + 9
-		expect(numSquares(841)).toBe(2); // 400 + 441
 		expect(numSquares(14)).toBe(3); // 1 + 4 + 9
 		expect(numSquares(62)).toBe(3); // 49 + 9 + 4
 		expect(numSquares(12)).toBe(3); // 4 + 4 + 4
-		expect(numSquares(76)).toBe(5); // 64 + 9 + 1 + 1 + 1
+		expect(numSquares(76)).toBe(3); // 4 + 36 + 36
+		expect(numSquares(48)).toBe(3); // 16 + 16 + 16
 	});
 });
